921c58cdc6d3d1b4a16d441443830d71b885f3fb
Haya Elnaggar <haya.ahmed05@eng-st.cu.edu.eg>
2025-08-11 20:35:21 +0300

    Stopwatch record minutes red

---
+++ src/main/java/example/Stopwatch/Stopwatch.java
@@ -0,0 +1,5 @@
+package example.Stopwatch;
+
+public class Stopwatch {
+    
+}
---
+++ src/test/java/example/Stopwatch/StopWatchTest.java
@@ -0,0 +1,23 @@
+package example.Stopwatch;
+import org.junit.jupiter.api.Test;
+//el steps for reference
+//TODO
+//test case l record minutes hat fail then h3ml record minutes then green w refactor law fe
+//test case negative input then red then hazabat el code then green then refactor law fe
+//if minutes reach 60 hours msh hay7salaha 7aga then hazabatha then refactor
+//if hours reach 24 days msh hay7salaha 7aga then hazabatha then refactor
+//daily working hours msh hay7sal 7aga l date then azabatha
+
+
+
+public class StopWatchTest {
+  @Test
+  void TestMinutes(){
+    //Arrange
+    Stopwatch stopwatch = new Stopwatch();
+    //Act
+    stopwatch.recordMinutes(5);
+    //Assert
+      assertEquals(5, stopwatch.getRecordedMinutes());
+  }
+}
