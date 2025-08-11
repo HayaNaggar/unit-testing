30c3160417b48d4521f581957649f56bb41ce05b
Haya Elnaggar <haya.ahmed05@eng-st.cu.edu.eg>
2025-08-11 21:00:44 +0300

    REFACTOR

--- src/main/java/example/Stopwatch/Stopwatch.java
+++ src/main/java/example/Stopwatch/Stopwatch.java
@@ -7,30 +7,20 @@ public class Stopwatch {
     int day=0;
     int workingHours=0;
 
-    public void recordMinutes(int minutes) {
-        if (minutes > 0) {
-     this.minutes = minutes;
-    }
-    else{
-            System.out.println("Invalid input. Minutes must be a positive number");
+  public void recordMinutes(int minutes) {
+    if (minutes > 0) {
+     this.minutes += minutes; 
+      if (this.minutes >= 60) {
+         this.hours += this.minutes / 60;
+         this.minutes = this.minutes % 60;
+        }
+        if (this.hours >= 24) {
+            this.day += this.hours / 24;
+            this.hours = this.hours % 24;
+        }
+    } else {
+        System.out.println("Invalid input. Minutes must be a positive number");
     }
 }
-    public int getRecordedMinutes(){
-    return this.minutes;
-    }
-    public int getRecordedHours() {
-        if (this.minutes >= 60) {
-            this.hours += this.minutes / 60;
-            this.minutes = this.minutes % 60;
-        }
-        return this.hours;
-    }
-    public int getRecordedDays() {
-         getRecordedHours(); 
-        if (this.hours >= 24) {
-        this.day += this.hours / 24;
-        this.hours = this.hours % 24;
-        }
-        return this.day;
-    }
+
 }
