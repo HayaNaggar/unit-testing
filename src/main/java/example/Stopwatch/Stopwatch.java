1a9a256cbb1112b82aad335936a652e02c2d65bb
Haya Elnaggar <haya.ahmed05@eng-st.cu.edu.eg>
2025-08-11 20:43:42 +0300

    GREEN RECORD MINUTES (NO REFACTOR NEEDED TILL NOW)

--- src/main/java/example/Stopwatch/Stopwatch.java
+++ src/main/java/example/Stopwatch/Stopwatch.java
@@ -1,5 +1,19 @@
 package example.Stopwatch;
 
+import java.lang.classfile.Signature;
+import java.lang.classfile.instruction.ThrowInstruction;
+
 public class Stopwatch {
-    
+    int hours;
+    int minutes;
+    int seconds;
+    int day;
+    int workingHours;
+
+    public void recordMinutes(int minutes) {
+     this.minutes = minutes;
+    }
+    public int getRecordedMinutes(){
+    return this.minutes;
+    }
 }
--- src/test/java/example/Stopwatch/StopWatchTest.java
+++ src/test/java/example/Stopwatch/StopWatchTest.java
@@ -1,4 +1,5 @@
 package example.Stopwatch;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 //el steps for reference
 //TODO
